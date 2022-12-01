e077b2b85f9c3bdff59dee5a14dff0b385ecb228
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2022-12-02 00:36:38 +0300

    Task4 complete

--- Seminar2/Task4.java
+++ Seminar2/Task4.java
@@ -4,6 +4,26 @@
 
 package Seminar2;
 
+import java.util.Scanner;
+
 public class Task4 {
-    
+    public static String UserInput(String messege) throws Exception {
+        System.out.println(messege);
+        String ex = "";
+        Scanner scanInp = new Scanner(System.in);
+        String name = scanInp.nextLine();
+        if (name == ex)
+            throw new Exception("Нельзя оставить строку пустой");
+        return name;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String name = UserInput("Как тебя зовут?");
+            System.out.println("Привет " + name + "!");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
