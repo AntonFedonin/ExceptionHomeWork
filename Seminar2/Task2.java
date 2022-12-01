7ba62bd6904d8d29af7919cb222617692d405b14
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2022-12-02 00:02:16 +0300

    Task2 complete

--- Seminar2/Task2.java
+++ Seminar2/Task2.java
@@ -1,5 +1,19 @@
+// Если необходимо, исправьте данный код (задание 2)
+
 package Seminar2;
 
 public class Task2 {
-    
+    public static void main(String[] args) {
+        try {
+            int intArray[] = { 1, 2, 33, 4, 5, 6, 7, 8, 9 };
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+
+    }
+
 }
+// Тут я не очень понял что нужно было исправить. В коде я просто добавил массив.
